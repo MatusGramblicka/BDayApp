0bfef352ce24989065991c1db367941068edb615
Gramblicka Matus <Gmt@int.kistler.com>
2024-05-21 12:28:04 +0200

    Authorized Swagger

--- BDayServer/BDayServer/Startup.cs
+++ BDayServer/BDayServer/Startup.cs
@@ -90,9 +90,33 @@ namespace BDayServer
             services.AddScoped<IEmailPreparator, EmailPreparator>();
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
+
+            services.AddSwaggerGen(option =>
             {
-                c.SwaggerDoc("v1", new OpenApiInfo {Title = "BDayServer", Version = "v1"});
+                option.SwaggerDoc("v1", new OpenApiInfo {Title = "BDayServer", Version = "v1"});
+                option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter a valid token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "Bearer"
+                });
+                option.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type=ReferenceType.SecurityScheme,
+                                Id="Bearer"
+                            }
+                        },
+                        new string[]{}
+                    }
+                });
             });
 
             services.AddRazorPages();
---
+++ BDayServer/Entities/SwaggerLoginDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Entities;
+
+public class SwaggerLoginDto
+{
+    public string Email { get; set; }
+    public string Password { get; set; }
+}
